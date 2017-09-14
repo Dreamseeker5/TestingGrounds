5d672b4eeb85576ae1ea16181c4b636c567a9d31
Edgar Hernández <riseofagamer@gmail.com>
2017-09-14 18:05:27 -0400

    Lecture 217 - Cpp AI Behavior Tree Tasks
    *Created a cpp class based on BTTaskNode
    *Replaced the ChooseNextWaypoint BP class by the new one in the BT.
    *Printed in the log to prove the cpp class works

--- Source/MyProject/MyProject.Build.cs
+++ Source/MyProject/MyProject.Build.cs
@@ -6,6 +6,6 @@ public class MyProject : ModuleRules
 {
 	public MyProject(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
